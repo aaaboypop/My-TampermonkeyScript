acf344a9a8f60c043c4091ab367cfedd98e73c13
pond_pop <badboydownload@gmail.com>
2023-02-11 00:46:55 +0700

    Update Aliexpress Button.user.js

--- Aliexpress Button.user.js
+++ Aliexpress Button.user.js
@@ -3,8 +3,8 @@
 // @version      0.3
 // @author       pond_pop
 // @description  aliexpress script
-// @updateURL    https://raw.githubusercontent.com/aaaboypop/My-TampermonkeyScript/main/Aliexpress%20Button.js
-// @downloadURL  https://raw.githubusercontent.com/aaaboypop/My-TampermonkeyScript/main/Aliexpress%20Button.js
+// @updateURL    https://github.com/aaaboypop/My-TampermonkeyScript/raw/main/Aliexpress%20Button.user.js
+// @downloadURL  https://github.com/aaaboypop/My-TampermonkeyScript/raw/main/Aliexpress%20Button.user.js
 // @match        https://*.aliexpress.com/item/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=aliexpress.com
 // @grant        GM_addStyle

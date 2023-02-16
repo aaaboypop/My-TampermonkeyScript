07af3f2f080178275f9ce388c34d70c8ee0847e9
pond_pop <badboydownload@gmail.com>
2023-02-16 20:26:13 +0700

    Update Aliexpress Button.user.js

--- Aliexpress Button.user.js
+++ Aliexpress Button.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Aliexpress Button
-// @version      0.3.1
+// @version      0.3.2
 // @author       pond_pop
 // @description  aliexpress script
 // @updateURL    https://raw.githubusercontent.com/aaaboypop/My-TampermonkeyScript/main/Aliexpress%20Button.js
@@ -21,18 +21,13 @@ const $q = d.querySelector.bind(document),
 
 GM_addStyle('.cpb{background-color: black; color: white; font-size: 36px; width: 240px; z-index:2;}');
 
-let a = NewButton("body", "CopyLink", "Copy Link", "position: fixed; bottom : 22px; left: 0px;")
-a.addEventListener('click', function(){getURL(a)})
-
-let a1 = NewButton("body", "ShortURL", "↗️", "position: fixed; bottom : 22px; left: 241px; width: 50px")
-a1.addEventListener('click', function(){getURL(a1, true)})
-
-let b = NewButton("body", "OpenImage", "Open Image", "position: fixed; bottom : 72px; left: 0px;")
-b.addEventListener('click', function(){OpenImageNewTab(b)})
+let a = NewButton("body", "CopyLink", "Copy Link", "position: fixed; bottom : 22px; left: 0px;", getURL,[], "Copied !")
+let a1 = NewButton("body", "ShortURL", "↗️", "position: fixed; bottom : 22px; left: 241px; width: 50px", getURL,[true])
+let b = NewButton("body", "OpenImage", "Open Image", "position: fixed; bottom : 72px; left: 0px;",OpenImageNewTab)
 
 // ----- ----- Function ----- -----
 
-function NewButton(selector, name, text, style){
+function NewButton(selector, name, text, style, fnCall, args=[], textOK="OK", textErr="Error !"){
     let el = $q(selector);
     let a = d.createElement('button');
     a.innerHTML = text;
@@ -40,6 +35,7 @@ function NewButton(selector, name, text, style){
     a.style = style;
     a.classList = 'cpb';
     el.prepend(a);
+    a.addEventListener('click', function(){ buttonEvents(a, fnCall, args, text, textOK, textErr)})
     return a
 }
 
@@ -48,27 +44,24 @@ function changeText(el, text, color){
     el.style.backgroundColor = color;
 }
 
-function OpenImageNewTab(el){
-    try {
-        let url
-        url = $q("#pdp-main-image > div > div > div.image-view-magnifier-wrap > img") ;
-            window.open(url.src);
-    } catch {
-        changeText(el, "Error !", "#B6001E");
-    }
-    setTimeout(changeText, 1200, el, "Open Image", "black");
+function buttonEvents(el, fnCall, args, textNormal, textOK, textErr="Error !"){
+    try {fnCall.apply(this, args); changeText(el, textOK, "#85B600")}
+    catch {changeText(el, textErr, "#85B600")}
+    setTimeout(changeText, 1200, el, textNormal, "black")
 }
 
-function getURL(el, open=false){
-    try {
-        let found = wloc.href.toString().match(/(http.*\.html)/);
-        let output = found[1]
-        if (open){
-            window.open(output, "_self")
-            return
-        }
-        navigator.clipboard.writeText(output)
-        changeText(el, "Copied !", "#85B600");
-    } catch { changeText(el, "Error !", "#B6001E"); }
-    setTimeout(changeText, 1200, el, "Copy Link", "black");
+function getURL(thisTab=false){
+    let found = wloc.href.toString().match(/(http.*\.html)/);
+    let output = found[1];
+    if (thisTab){
+        window.open(output, "_self")
+        return
+    }
+    navigator.clipboard.writeText(output)
+}
+
+function OpenImageNewTab(){
+    let url
+    url = $q("#pdp-main-image > div > div > div.image-view-magnifier-wrap > img") ;
+        window.open(url.src);
 }
